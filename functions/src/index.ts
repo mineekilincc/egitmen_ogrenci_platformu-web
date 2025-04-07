15ac652c72a579427337eee4c4a1081ee99ca840
mineekilincc <kilincmine658@gmail.com>
2025-04-08 00:04:38 +0300

    Initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+// Basit bir HTTP fonksiyonu örneği:
+export const helloWorld = onRequest((req, res) => {
+  logger.info("Hello world function is being called");
+  res.send("Hello, World!");
+});
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
